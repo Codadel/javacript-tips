f703eee8b21764212efaeb65cc4451c8523270a4
Adel Denert <59997728+Codadel@users.noreply.github.com>
2020-01-24 17:56:51 +0100

    Update avoidInputScroll.js

--- avoidInputScroll.js
+++ avoidInputScroll.js
@@ -1,6 +1,15 @@
+// avoid scroll on inputs
 window.addEventListener('wheel', e => {
   const target = e.target as HTMLElement;
   if (target.tagName === 'INPUT') {
+    target.blur();
+  }
+});
+
+// avoid scroll on inputs type number
+window.addEventListener('wheel', e => {
+  const target = e.target as HTMLInputElement;
+  if (target.tagName === 'INPUT' && target.type === 'number') {
     target.blur();
   }
 });
